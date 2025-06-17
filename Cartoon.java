80ae939cbb5f2cae2f621e7f6fa8e38016e98433
Shravani <shravanin2003@gmail.com>
2025-06-17 14:05:34 +0530

    sop

---
+++ Cartoon.java
@@ -0,0 +1,7 @@
+class Cartoon {
+public static void main(String Dora[]){
+System.out.println("Dora is famous Cartoon");
+System.out.println("We used to watch this Cartoon during our childhood");
+System.out.println("We used to watch Dora in kushi tv channel");
+}
+}
